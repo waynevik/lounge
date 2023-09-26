b50a66f9261b1d06237ad97f7f1df43aaeebdae7
wayne <wayne@rhino.rh.com>
2023-09-26 11:32:01 +0300

    Cross origin changes again

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     'http:/127.0.0.1:3000', 
     'http://localhost:3000',
     'https://academicslounge.co.ke/',
-    'https://portal.academicslounge.co.ke/'
+    'https://portal.academicslounge.co.ke'
 ];
 
 
